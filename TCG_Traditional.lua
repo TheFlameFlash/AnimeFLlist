670f7cd3c3038b6a28639be3ce9a42ccf0aadd27
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-05-15 08:21:10 -0300

    Each list

---
+++ TCG_Traditional.lua
@@ -0,0 +1,184 @@
+
+!2018.2 Traditional
+--[[
+#Forbidden Traditional
+#Limited Traditional
+7902349 1 --Left Arm of the forbidden one						MAIN DECK MONSTERS
+44519536 1 --Left Leg of the forbidden one
+70903634 1 --Right Arm of the forbidden one
+8124921 1 --Right Leg of the forbidden one
+65518099 1 --Qliporth Scout
+33396948 1 --Exxodia the Forbidden One
+53804307 1 --Blaster, Dragon Ruler of Infernos
+82301904 1 --Chaos Emperor Dragon - Envoy of the End
+34124316 1 --Cyber Jar
+69015963 1 --Cyber Stein
+56570271 1 --Destiny Hero - Disk Commander
+8903700 1 --Djinn Releaser of Rituals
+40044918 1 --Elemental Hero Stratos
+78706415 1 --Fiber Jar
+93369354 1 --Fishborg Blaster
+34206604 1 --Magical Scientist
+31178212 1 --Majespecter Unicorn - Kirin
+21593977 1 --Makyura the Destructor
+96782886 1 --Mind Master
+33508719 1 --Morphing Jar
+79106360 1 --Morphing Jar #2
+68819554 1 --Performage Damage Juggler
+7563579 1 --Performage PlushFire
+17330916 1 --Performapal Monkeyboard
+90411554 1 --Redox, Dragon Ruler of Boulders
+90307777 1 --Shurit, Strategist of the Nekroz
+20663556 1 --Substitoad
+89399912 1 --Tempest, Dragon Ruler of Storms
+88071625 1 --The Tyrant Neptune
+26400609 1 --Tidal, Dragon Ruler of Waterfalls
+33184167 1 --Tribe-Infecting Virus
+44910027 1 --Victory Dragon
+58984738 1 --Dinomight Knight, the True Dracofighter
+30539496 1 --True King Lithosagym, the Disaster
+3078576 1 --Yata-Garasu
+15341821 1 --Dandylion
+65192027 1 --Dark Armed Dragon
+78868119 1 --Deep Sea Diva
+64034255 1 --Genex Ally birdman
+20758643 1 --Graff, Malembrance of the Burning Abyss
+99177923 1 --Infernity Archfiend
+68184115 1 --Inzektor DragonFly
+69207766 1 --Inzektor Hornet
+16226786 1 --Night Assailant
+88264978 1 --Red-eyes Darkness Metal Dragon
+61740673 1 --Imperial Order
+10802915 1 --Tour Guide From the Underworld
+81275020 1 --Speedroid Terrortop
+57143342 1 --Cir, Malebranche of the Burning Abyss
+55885348 1 --Kozmo Dark Destroyer
+96570609 1 --Ether the Heavenly monarch
+23434538 1 --Maxx "C"
+38572779 1--Miscellaneousaurus
+78872731 1 --Zoodiac Ratpier
+4474060 1 --SPYRAL Gear - Drone
+78080961 1 --SPYRAL Quik-Fix
+45222299 1 --Evigishki Gustkraken
+11877465 1 --Evigishki Mind Augus
+26674724 1 --Nekroz of Brionac
+89463537 1 --Nekroz of Unicore
+49684352 1 --Double Iris Magician
+40318957 1 --Performapal Skullcrobat Joker
+57421866 1 --Level Eater
+75732622 1 --Grinder Golem
+9929398 1 --Blackwing - Gofu the Vague Shadow
+76794549 1 --Astrograph Sorcerer
+21377582 1 --Master Peace, The True Dracoslaying King
+23558733 1 --Phoenixian Cluster Amaryllis				
+18239909 1 --Ignister Prominence, the Blasting Dracoslayer 		EXTRA DECK MONSTERS
+48063985 1 --Ritual Beast Ulti-Cannahawk
+39512984 1 --Gem-Knight Master Diamond
+70583986 1 --Dewloren, Tiger King of the Ice Barrier
+52687916 1 --Trishula, Dragon of the Ice Barrier
+27552504 1 --Beatrice, Lady of Eternal
+20366274 1 --El Shaddoll Construct
+17412721 1 --Elder Entity Norden
+65536818 1 --Denglong, First of the Yang Zing
+46772449 1 --Evilswarm Exciton Knight
+34086406 1 --Lavalval Chain
+54719828 1 --Number 16: Shock Master
+18326736 1 --Tellarknight Ptolomaeus
+81122844 1 --Wind-Up Carrier Zenmaity
+581014 1 --Daigusto Emeral
+48905153 1 --Zoodiac Drident
+85115440 1 --Zoodiac Broadbull
+5043010 1 --Firewall Dragon	
+43387895 1 --Supreme King Dragon Starving Venom
+25862681 1 --Ancient Fairy Dragon					
+14087893 1 --Book of Moon						SPELLS CARDS
+69243953 1 --Butterfly Dagger - Elma
+72892473 1 --Card Destruction
+57953380 1 --Card of Safe Return
+4031928 1 --Change of Heart
+67616300 1 --Chicken Game
+60682203 1 --Cold Wave
+17375316 1 --Confiscation
+44763025 1 --Deliquent Duo
+23557835 1 --Dimension Fusion
+42703248 1 --Giant Trunade
+79571449 1 --Graceful Charity
+18144507 1 --Harpie's Feather Duster
+18144506 1 --Harpie's Feather Duster
+35059553 1 --Kaiser Colloseum
+19613556 1 --Heavy Storm
+85602018 1 --Last Will
+46411259 1 --Metamorphosis
+83764718 1 --Monster Reborn
+83764719 1 --Monster Reborn
+74191942 1 --Painful Choice
+67169062 1 --Pot of Avarice
+55144522 1 --Pot of greed
+34906152 1 --Mass Driver
+41482598 1 --Mirage of Nightmare
+70828912 1 --Premature Burial
+45986603 1 --Snatch Steal
+46448938 1 --Spellbook of Judgement
+48130397 1 --Super Polymerization
+27770341 1 --Super Rejuvenation
+42829885 1 --The Forceful Sentry
+11110587 1 --That Grass Looks Greener
+81674782 1 --Dimensional Fissure
+95308449 1 --Final Countdown
+81439173 1 --Foolish Burial
+66957584 1 --Infernity Launcher
+23171610 1 --Limiter Removal
+43040603 1 --Monster Gate
+33782437 1 --One Day of Peace
+2295440 1 --One for One
+12580477 1 --Raigeki
+53129443 1 --Dark Hole
+32807846 1 --Reinforcement of the Army
+74845897 1 --Rekindling
+72405967 1 --Royal Tribute
+54447022 1 --Soul Charge
+45305419 1 --Symbol of Heritage
+14733538 1 --Draco Face-off
+73468603 1 --Set Rotation
+58577036 1 --Reasoning
+99330325 1 --Interrupted Kaiju Slumber
+27970830 1 --Gateway of the Six
+70368879 1 --Upstart Goblin
+67723438 1 --Emergency Teleporter
+22842126 1 --Phanteism of the Monarchs
+79844764 1 --The Monarchs Stormforth
+54631665 1 --SPYRAL Resort	
+15854426 1 --Divine Wind of the Mist Valley
+94220427 1 --Rank-Up-Magic Argent Chaos Force				
+28566710 1 --Last Turn									TRAPS CARDS
+27174286 1 --Return from the Different Dimension
+93016201 1 --Royal Oppression
+57585212 1 --Self-Destruct Button
+3280747 1 --Sixth Sense
+41420027 1 --Solemn Judgement
+35316708 1 --Time Seal
+64697231 1 --Trap Dustshot
+80604091 1 --Ultimate Offering
+80604092 1 --Ultimate Offering
+5851097 1 --Vanity's Emptiness
+54974237 1 --Eradicator Epidemic Virus
+32723153 1 --Magical Explosion
+35125879 1 --True King's Return
+82732705 1 --Skill Drain
+73599290 1 --Soul Drain
+30241314 1 --Macro Cosmos
+17078030 1 --Wall of Revealing light
+9059700 1 --Infernity Barrier
+84749824 1 --Solemn Warning
+91623717 1 --Chain Strike
+
+#semi limited Traditional
+27279764 2 --Apoqliphort Towers
+28297833 2 --Necroface
+59297550 2 --Wind-up Magician
+92746535 2 --Luster Pendulum, the Dracoslayer
+90953320 2 --T.G. Hyper Librarian	
+48976825 2 --Burial from a Different Dimension
+83555666 2 --Ring of Destruction
+83555667 2 --Ring of Destruction
+--]]
---
+++ Worlds.lua
@@ -0,0 +1,221 @@
+ !2018.4 Worlds
+ --[[
+#Forbidden Worlds
+53804307 0 --Blaster, Dragon Ruler of Infernos						MAIN DECK MONSTERS
+89399912 0 --Tempest, Dragon Ruler of Storms
+26400609 0 --Tidal, Dragon Ruler of Waterfalls
+90411554 0 --Redox, Dragon Ruler of Boulders
+82301904 0 --Chaos Emperor Dragon - Envoy of the End
+34124316 0 --Cyber Jar
+69015963 0 --Cyber Stein
+56570271 0 --Destiny Hero - Disk Commander
+08903700 0 --Djinn Releaser of Rituals
+40044918 0 --Elemental Hero Stratos
+78706415 0 --Fiber Jar
+93369354 0 --Fishborg Blaster
+34206604 0 --Magical Scientist
+21593977 0 --Makyura the Destructor
+96782886 0 --Mind Master
+33508719 0 --Morphing Jar
+79106360 0 --Morphing Jar #2
+90307777 0 --Shurit, Strategist of the Nekroz
+88071625 0 --The Tyrant Neptune
+20663556 0 --Substitoad
+33184167 0 --Tribe-Infecting Virus
+44910027 0 --Victory Dragon
+03078576 0 --Yata-Garasu
+17330916 0 --Performapal Monkeyboard
+07563579 0 --Performage PlushFire
+68819554 0 --Performage Damage Juggler
+57421866 0 --Level Eater
+16923472 0 --Wind-up Hunter
+31178212 0 --Majespecter Unicorn - Kirin
+21377582 0 --Master Peace, The True Dracoslaying King
+58984738 0 --Dinomight Knight, the True Dracofighter
+61468779 0 --Grandsoil the Elemental Lord
+30539496 0 --True King Lithosagym, the Disaster
+09929398 0 --Blackwing - Gofu the Vague Shadow
+15341821 0 --Dandylion
+49684352 0 --Double Iris Magician
+23434538 0 --Maxx "C"
+40318957 0 --Performapal Skullcrobat Joker
+23558733 0 --Phoenixian Cluster Amaryllis
+20366274 0 --El Shaddoll Construct									EXTRA DECK MONSTERS
+17412721 0 --Elder Entity Norden
+25862681 0 --Ancient Fairy Dragon
+65536818 0 --Denglong, First of the Yang Zing	
+46772449 0 --Evilswarm Exciton Knight
+18326736 0 --Tellarknight Ptolomaeus
+34086406 0 --Lavalval Chain
+54719828 0 --Number 16: Shock Master
+81122844 0 --Wind-Up Carrier Zenmaity
+48905153 0 --Zoodiac Drident
+85115440 0 --Zoodiac Broadbull
+581014 0 --Daigusto Emeral
+69243953 0 --Butterfly Dagger - Elma								SPELLS CARDS
+67616300 0 --Chicken Game
+12580477 0 --Raigeki
+35059553 0 --Kaiser Colloseum
+72892473 0 --Card Destruction
+57953380 0 --Card of Safe Return
+04031928 0 --Change of Heart
+60682203 0 --Cold Wave
+17375316 0 --Confiscation
+44763025 0 --Deliquent Duo
+23557835 0 --Dimension Fusion
+42703248 0 --Giant Trunade
+79571449 0 --Graceful Charity
+18144507 0 --Harpie's Feather Duster
+18144506 0 --Harpie's Feather Duster
+42829885 0 --The Forceful Sentry
+19613556 0 --Heavy Storm
+85602018 0 --Last Will
+46411259 0 --Metamorphosis
+74191942 0 --Painful Choice
+67169062 0 --Pot of Avarice
+55144522 0 --Pot of greed
+34906152 0 --Mass Driver
+41482598 0 --Mirage of Nightmare
+46060017 0 --Zoodiac Barrage
+70828912 0 --Premature Burial
+45986603 0 --Snatch Steal
+46448938 0 --Spellbook of Judgement
+48130397 0 --Super Polymerization
+27770341 0 --Super Rejuvenation
+13035077 0 --Dragonic Diagram
+28566710 0 --Last Turn											TRAPS CARDS
+27174286 0 --Return from the Different Dimension
+93016201 0 --Royal Oppression
+17178486 0 --Life Equalizer
+57585212 0 --Self-Destruct Button
+03280747 0 --Sixth Sense
+35316708 0 --Time Seal
+64697231 0 --Trap Dustshot
+80604091 0 --Ultimate Offering
+05851097 0 --Vanity's Emptiness
+#Limited Worlds
+07902349 1 --Left Arm of the forbidden one						MAIN DECK MONSTERS
+44519536 1 --Left Leg of the forbidden one
+70903634 1 --Right Arm of the forbidden one
+08124921 1 --Right Leg of the forbidden one
+65518099 1 --Qliporth Scout
+33396948 1 --Exxodia the Forbidden One
+65192027 1 --Dark Armed Dragon
+78868119 1 --Deep Sea Diva
+64034255 1 --Genex Ally birdman
+50720316 1 --Elemental Hero Shadow Mist
+20758643 1 --Graff, Malembrance of the Burning Abyss
+99177923 1 --Infernity Archfiend
+68184115 1 --Inzektor DragonFly
+69207766 1 --Inzektor Hornet
+28297833 1 --Necroface
+80344569 1 --Neo-Spacian Grand Mole
+16226786 1 --Night Assailant
+78872731 1 --Zoodiac Ratpier
+55623480 1 --Fairy Tail - Snow
+88264978 1 --Red-eyes Darkness Metal Dragon
+10802915 1 --Tour Guide From the Underworld
+81275020 1 --Speedroid Terrortop
+57143342 1 --Cir, Malebranche of the Burning Abyss
+55885348 1 --Kozmo Dark Destroyer
+96570609 1 --Ether the Heavenly monarch
+44335251 1 --Souleating Oviraptor
+36042004 1 --Babycerasaurus
+22499034 1 --Ignis Heat, the True Dracowarrior
+38572779 1 --Miscellaneousaurus
+92746535 1 --Luster Pendulum, the Dracoslayer
+50720316 1 --Elemental Hero Shadow Mist
+4474060 1 --SPYRAL Gear - Drone
+78080961 1 --SPYRAL Quik-Fix
+9047460 1 --Blackwing - Steam the Cloak
+27279764 1 --Apoqliphort Towers
+75732622 1 --Grinder Golem
+69610326 1 --Supreme King Dragon Darkwurm
+48686504 1 --Lonefire Blossom
+35272499 1 --Predaplant Ophrys Scorpio
+45222299 1 --Evigishki Gustkraken
+11877465 1 --Evigishki Mind Augus
+26674724 1 --Nekroz of Brionac
+89463537 1 --Nekroz of Unicore
+1561110 1 --ABC-Dragon Buster 									EXTRA DECK MONSTERS
+94977269 1 --El Shaddoll Winda
+75286621 1 --Invoked Mechaba	
+18239909 1 --Ignister Prominence, the Blasting Dracoslayer
+48063985 1 --Ritual Beast Ulti-Cannahawk
+74586817 1 --PSY-Framelord Omega
+83531441 1 --Dante, Traveler of the Burning Abyss
+90809975 1 --Toadally Awesome
+06602300 1 --Blaze Fenix, The Burning Bombardment Bird
+70583986 1 --Dewloren, Tiger King of the Ice Barrier
+52687916 1 --Trishula, Dragon of the Ice Barrier
+90953320 1 --T.G. Hyper Librarian
+27552504 1 --Beatrice, Lady of Eternal
+5043010 1 --Firewall Dragon				
+14087893 1 --Book of Moon 							SPELLS CARDS				
+11110587 1 --That Grass Looks Greener
+97211663 1 --Nekroz Cycle
+81674782 1 --Dimensional Fissure
+95308449 1 --Final Countdown
+81439173 1 --Foolish Burial
+66957584 1 --Infernity Launcher
+23171610 1 --Limiter Removal
+53129443 1 --Dark Hole
+43040603 1 --Monster Gate
+33782437 1 --One Day of Peace
+02295440 1 --One for one
+32807846 1 --Reinforcement of the Army
+74845897 1 --Rekindling
+72405967 1 --Royal Tribute
+27970830 1 --Gateway of the Six
+54447022 1 --Soul Charge
+45305419 1 --Symbol of Heritage
+14733538 1 --Draco Face-off
+73468603 1 --Set Rotation
+93600443 1 --Mask Change 2
+58577036 1 --Reasoning
+99330325 1 --Interrupted Kaiju Slumber
+23701465 1 --Primal Seed
+70368879 1 --Upstart Goblin
+67723438 1 --Emergency Teleporter
+73628505 1 --Terraforming
+89739383 1 --Spellbook of Secrets
+53208660 1 --Pendulum Call
+22842126 1 --Phanteism of the Monarchs
+79844764 1 --The Monarchs Stormforth
+15854426 1 --Divine Wind of the Mist Valley
+54631665 1 --SPYRAL Resort
+83764718 1 --Monster Reborn
+83764719 1 --Monster Reborn	
+23314220 1 --Spellbook of Knowledge			
+94192409 1 --Compulsory Evacuation Device 			TRAPS CARDS
+54974237 1 --Eradicator Epidemic Virus
+32723153 1 --Magical Explosion
+36468556 1 --Ceasefire
+21076084 1 --Trickstar Reincarnation
+61740673 1 --Imperial Order
+35125879 1 --True King's Return
+83555666 1 --Ring of Destruction
+83555667 1 --Ring of Destruction
+82732705 1 --Skill Drain
+73599290 1 --Soul Drain
+30241314 1 --Macro Cosmos
+17078030 1 --Wall of Revealing light
+09059700 1 --Infernity Barrier
+84749824 1 --Solemn Warning
+41420027 1 --Solemn Judgement
+#Semi Limited Worlds
+74311226 2 --Atlantean Dragoons
+59297550 2 --Wind-up Magician
+43722862 2 --Windwitch – Ice Bell
+41386308 2 --Mathematician
+14558127 2 --Ash Blossom & Joyous Spring
+86120751 2 --Aleister the Invoker
+59750328 2 --Card of Demise
+47325505 2 --Fossil Dig
+48976825 2 --Burial from a Different Dimension
+73915051 2 --Scapegoat
+49430782 2 --True Draco Heritage
+91623717 2 --Chain Strike
+66399653 2 --Union Hangar
+40605147 2 --Solemn Strike
+--]]
